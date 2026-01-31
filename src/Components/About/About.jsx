19e88d2a6f0f09eede68c24adb1d89970f7ba064
Arsalan Muteeb Mir <arsalan@Arsalan.Arsalan>
2026-01-31 21:22:51 +0530

    cvchange

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -42,32 +42,32 @@ const About = () => {
             Learning new technologies and improving my problem-solving skills keeps me motivated every day.
           </p>
           {/* Resume button */}
-          <a href="https://drive.google.com/file/d/1dQYHwEXpbTzFlst1VrkxxrkdUy0ZfBSU/view?usp=sharing"
-          target='_blank'
-          rel='noopener noreferrer'
-          className='inline-block text-white py-3 px-8 rounded-full text-lg font-bold transition duration-300 transform hover:scale-105 '
-          style={{
-            background:'linear-gradient(90deg, #8245ec, #a855f7)',
-            boxShadow: '0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec',
-          }}>Download CV
+          <a href="https://drive.google.com/file/d/1oKyxLdgJzCcoqUhijzqfchU-N716i2D7/view?usp=sharing"
+            target='_blank'
+            rel='noopener noreferrer'
+            className='inline-block text-white py-3 px-8 rounded-full text-lg font-bold transition duration-300 transform hover:scale-105 '
+            style={{
+              background: 'linear-gradient(90deg, #8245ec, #a855f7)',
+              boxShadow: '0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec',
+            }}>Download CV
           </a>
         </div>
         {/* Right side */}
         <div className='md:w-1/2 flex justify-center md:justify-center'>
-        <Tilt className='w-48 h-48 sm:w-64 sm:h-64 md:w-[30rem] md:h-[30rem] border-4 border-purple-700 rounded-full'
-         tiltMaxAngleX={20}
-         tiltMaxAngleY={20}
-         perspective={1200}
-         scale={1.05}
-         transitionSpeed={1000}
-         gyroscope={true}
+          <Tilt className='w-48 h-48 sm:w-64 sm:h-64 md:w-[30rem] md:h-[30rem] border-4 border-purple-700 rounded-full'
+            tiltMaxAngleX={20}
+            tiltMaxAngleY={20}
+            perspective={1200}
+            scale={1.05}
+            transitionSpeed={1000}
+            gyroscope={true}
 
-         >
-          <img src={myimg} alt="Arsalan Mir"
-          className='w-full h-full rounded-full  object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]'
-          />
-        </Tilt>
-          
+          >
+            <img src={myimg} alt="Arsalan Mir"
+              className='w-full h-full rounded-full  object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]'
+            />
+          </Tilt>
+
         </div>
       </div>
 
